3bac25e52c290ace5f6cad1908c7bcdd4bf6291a
aaqilRamzeen <39322657+aaqilRamzeen@users.noreply.github.com>
2019-06-23 17:32:43 +0530

    Sample data for the Products table

---
+++ app/containers/Tables/demos/productData.js
@@ -0,0 +1,79 @@
+export const anchorTable = [
+  {
+    name: 'id',
+    label: 'Product ID',
+    initialValue: '',
+    width: '120',
+    hidden: false
+  }, {
+    name: 'product',
+    label: 'Product Name',
+    initialValue: null,
+    width: 'auto',
+    hidden: false
+  }, {
+    name: 'category',
+    label: 'Category',
+    initialValue: null,
+    width: 'auto',
+    hidden: false
+  }, {
+    name: 'varient',
+    label: 'HasVarient',
+    initialValue: true,
+    width: '120',
+    hidden: false
+  }, {
+    name: 'status',
+    label: 'Delete',
+    initialValue: true,
+    width: '100',
+    hidden: false,
+  }, 
+];
+
+export const dataApi = [
+  {
+    id: '1',
+    product: 'Kelloggs Berries Yoghurty Bar',
+    category: 'Cereal',
+    varient: 'false',
+    status: 'option1',
+  }, {
+    id: '2',
+    product: 'Maliban Chick Bits',
+    category: 'Biscuits',
+    varient: 'true',
+    status: 'option1',
+  }, {
+    id: '3',
+    product: 'Finagle Bread Crumbs',
+    category: 'Flour',
+    varient: 'true',
+    status: 'option1',
+  }, {
+    id: '4',
+    product: 'Kelloggs Corn Flakes',
+    category: 'Cereal',
+    varient: 'true',
+    status: 'option1',
+  }, {
+    id: '5',
+    product: 'Fortune Sunflower Oil',
+    category: 'Oils & Fats',
+    varient: 'true',
+    status: 'option1',
+  }, {
+    id: '6',
+    product: 'Nescafe Classic',
+    category: 'Coffee',
+    varient: 'true',
+    status: 'option1',
+  }, {
+    id: '7',
+    product: 'Kandos Bar Cashew Nut',
+    category: 'Confectionery',
+    varient: 'false',
+    status: 'option1',
+  }
+];
